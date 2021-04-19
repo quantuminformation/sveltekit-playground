2cedf9629d6ba4b5655c63ff9b0ea5c0967b9b89
Nikos Katsikanis <nkatsikanis@gmail.com>
2021-04-19 14:34:54 +0200

    add route

--- .svelte/dev/generated/manifest.js
+++ .svelte/dev/generated/manifest.js
@@ -1,7 +1,8 @@
 import * as layout from "../../../src/routes/$layout.svelte";
 
 const components = [
-	() => import("../../../src/routes/index.svelte")
+	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/test.svelte")
 ];
 
 const d = decodeURIComponent;
@@ -9,7 +10,10 @@ const empty = () => ({});
 
 export const routes = [
 	// src/routes/index.svelte
-[/^\/$/, [components[0]]]
+[/^\/$/, [components[0]]],
+
+// src/routes/test.svelte
+[/^\/test\/?$/, [components[1]]]
 ];
 
 export { layout };
